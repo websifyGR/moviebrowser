71eb40212fbe8a2fdf345052375756d5f84d8d22
websifyGR <email@websify.gr>
2023-03-05 03:33:31 +0200

    update path

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movies-api/precache-manifest.9fdff99886b8fc105525995691f3e600.js"
+  "/movies/build/precache-manifest.7901d427090ef4fc73acb54b98bd2a99.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/movies-api/index.html", {
+workbox.routing.registerNavigationRoute("/movies/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
